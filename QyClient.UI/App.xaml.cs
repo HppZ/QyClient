09c57fb88b3ee591bd8707e5a33f0bf9255e62b2
hp <haopeng__@outlook.com>
2017-11-19 22:56:57 +0800

    1

--- QyClient.UI/App.xaml.cs
+++ QyClient.UI/App.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using QyClient.UI.View;
+using QyClient.UI.View.MainView;
 
 namespace QyClient.UI
 {
