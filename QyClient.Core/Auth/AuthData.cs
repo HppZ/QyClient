37fbc8f0bd09f609a003f2cc6627282773c5edeb
HppZ <haopeng__@outlook.com>
2017-12-13 16:51:04 +0800

    1

---
+++ QyClient.Core/Auth/AuthData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QyClient.Core.Model;
+
+namespace QyClient.Core.Auth
+{
+    internal static class AuthData
+    {
+        public static event Action AuthDataUpdated;
+
+        public static string account { get; private set; }
+        public static string cookie { get; private set; }
+
+        public static void Update(Account userAccount)
+        {
+            account = "";
+            cookie = "";
+
+            AuthDataUpdated?.Invoke();
+        }
+
+    }
+}
--- QyClient.Core/Controller/AccountController.cs
+++ QyClient.Core/Controller/AccountController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QyClient.Core.Auth;
 using QyClient.Core.DAL.Interface;
+using QyClient.Core.Model;
+using QyClient.Core.Navigation.INavigator;
 
 namespace QyClient.Core.Controller
 {
    public class AccountController
-    {
+   {
+       private IAccountRepository _repository;
         public AccountController(IAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Login()
+        {
+
+
+        }
+
+        public void Logout()
         {
             
         }
+
+
+        private void UpdateAuth()
+        {
+            var account = new Account();
+            AuthData.Update(account);
+        }
+
+
+
     }
 }
